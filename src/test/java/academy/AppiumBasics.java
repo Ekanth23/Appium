a6741d8c6af4bb054e51adc9f5ebf632795562be
Ekanth23 <ekanthn@gmail.com>
2026-04-23 16:16:50 +0530

    BaseTest class added

--- src/test/java/academy/AppiumBasics.java
+++ src/test/java/academy/AppiumBasics.java
@@ -4,30 +4,35 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
 
-public class AppiumBasics {
+public class AppiumBasics extends BaseTest{
 	
 	@Test
 	public void AppiumTest() throws MalformedURLException, URISyntaxException
 	{
 		//code to start the server programmatically 
-		AppiumDriverLocalService service = new AppiumServiceBuilder().withAppiumJS(new File("C:\\Users\\Royal\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js"))
-		.withIPAddress("127.0.0.1").usingPort(4723).build();
-		service.start();
-		
-		UiAutomator2Options options = new UiAutomator2Options(); 
-		options.setDeviceName("Pixel 7");
-		options.setApp(" ");
-		AndroidDriver driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), options);
-		driver.quit();
-		
-		service.stop(); 
+			
+		//id, xpath, accessibilityid, classname, androidUIAutomator
+		//configureAppium() method to start server will be executed first 
+		//click Preference
+		driver.findElement(AppiumBy.accessibilityId("Preference")).click();
+		driver.findElement(By.xpath("//android.widget.TextView[@content-desc='3. Preference dependencies']")).click();
+		//click on the wifi checkbox 
+		driver.findElement(By.xpath("//android.widget.CheckBox[@resource-id='android:id/checkbox']")).click();
+		//click on the wifisetting
+		driver.findElement(By.xpath("(//android.widget.RelativeLayout)[2]")).click();
+		//xpath syntax
+		//tagName[@attribu
+	
+ 
 	}
 
 }
